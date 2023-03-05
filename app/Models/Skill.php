87e2a0908e14b72e23b050108025657a12dd218f
Reniki <hello@reniki.com>
2023-03-05 15:31:58 +0100

    enum SkillStatus: todo, in progress, done | pa migrate

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Dog;
+use App\Enums\SkillStatus;
 use App\Models\SkillCategory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +12,9 @@ class Skill extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'skill_category_id'];
+    protected $fillable = ['name', 'description', 'skill_category_id', 'status'];
+
+    protected $casts = ['status' => SkillStatus::class];
 
     public function dogs()
     {
--- database/migrations/2023_03_05_131523_create_dog_skill_table.php
+++ database/migrations/2023_03_05_131523_create_dog_skill_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Enums\SkillStatus;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('dog_id')->constrained()->cascadeOnDelete();
             $table->foreignId('skill_id')->constrained()->cascadeOnDelete();
+            $table->string('status')->default(SkillStatus::TODO->value); // todo, in_progress, done
             $table->timestamps();
         });
     }
