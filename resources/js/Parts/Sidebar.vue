8901b1238c786a5e00067d78fc14a13217271cc9
Reniki <hello@reniki.com>
2023-03-06 09:29:19 +0100

    sidebar css, delete nav heading

--- resources/js/Parts/Sidebar.vue
+++ resources/js/Parts/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4">
+  <div class="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4 px-2 min-h-screen">
     <div class="flex flex-shrink-0 items-center px-4">
       <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
     </div>
@@ -13,9 +13,9 @@
             </a>
           </div>
           <Disclosure as="div" v-else class="space-y-1" v-slot="{ open }">
-            <DisclosureButton :class="[item.current ? 'bg-gray-100 text-gray-900' : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex w-full items-center rounded-md py-2 pl-2 pr-1 text-left text-sm font-medium focus:outline-none focus:ring-2 focus:ring-indigo-500']">
+            <DisclosureButton :class="[item.current ? 'bg-gray-100 text-gray-900' : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex w-full items-center rounded-md py-2 pr-1 text-left text-sm font-medium focus:outline-none focus:ring-2 focus:ring-indigo-500']">
               <component :is="item.icon" class="mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-              <span class="flex-1">{{ item.name }}</span>
+              <span class="flex-1 pr-8">{{ item.name }}</span>
               <svg :class="[open ? 'rotate-90 text-gray-400' : 'text-gray-300', 'ml-3 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400']" viewBox="0 0 20 20" aria-hidden="true">
                 <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
               </svg>
