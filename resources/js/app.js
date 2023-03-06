8af00d58b2ef361374828b2def77731eb54276e0
Reniki <hello@reniki.com>
2023-03-06 11:08:39 +0100

    create Pinia instance in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { Head, Link } from '@inertiajs/vue3';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia()
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -16,6 +19,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(pinia)
             .component('Head', Head)
             .component('Link', Link)
             .mount(el);
