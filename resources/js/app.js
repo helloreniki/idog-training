619304c7b807de57ce5c64f40b94692175b21bff
Reniki <hello@reniki.com>
2023-03-08 21:38:25 +0100

    persistent default layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,28 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { Head, Link } from '@inertiajs/vue3';
 import { createPinia } from 'pinia';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 
 const pinia = createPinia()
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
+resolve: name => {
+  const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+  let page = pages[`./Pages/${name}.vue`]
+  page.default.layout = page.default.layout || AuthenticatedLayout
+  return page
+},
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: name => {
+      const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+      let page = pages[`./Pages/${name}.vue`]
+      page.default.layout = page.default.layout || AuthenticatedLayout
+      return page
+    },
+    // resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
