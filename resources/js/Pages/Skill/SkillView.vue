d717ded4dbc1c5ea8acd07ce0722b3848a138d3c
Reniki <hello@reniki.com>
2023-03-09 17:14:08 +0100

    SkillView: show categories, onclick show category skills

--- resources/js/Pages/Skill/SkillView.vue
+++ resources/js/Pages/Skill/SkillView.vue
@@ -1,10 +1,28 @@
 <template>
-  <AuthenticatedLayout>
-    SKILL VIEW
-  </AuthenticatedLayout>
+  <div v-for="categoryObj, index in dogStore.chosenDog.categories" :key="index" class="">
+    <div @click="openCategory(categoryObj)" class="cursor-pointer">{{index}}</div>
+  </div>
+  <div v-if="openedCategory" class="mt-8">
+    <div v-for="skill in openedCategory" :key="skill.id">{{ skill.name }}</div>
+  </div>
 </template>
 
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useDogStore } from '../../../Stores/dog';
+import { ref } from 'vue';
+
+const dogStore = useDogStore();
+
+const categories = dogStore.chosenDog.categories
+
+// console.log(Object.keys(dogStore.chosenDog.categories))
+
+// i need categories keys
+const openedCategory = ref(null)
+
+function openCategory(categoryObj) {
+  openedCategory.value = categoryObj
+  console.log(openedCategory.value)
+}
 
 </script>
