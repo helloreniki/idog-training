9624788872dc6a4212c3004d9dbdec7eeefd65e0
Reniki <hello@reniki.com>
2023-03-13 18:34:46 +0100

    nav class for chosenDog

--- resources/js/Parts/DogNav.vue
+++ resources/js/Parts/DogNav.vue
@@ -2,11 +2,14 @@
   <div class="bg-white w-full flex gap-8 px-4 py-3 justify-between items-center border-b">
     <div v-if="dogStore.chosenDog" class="font-semibold">ChosenDog: {{ dogStore.chosenDog.name }}</div>
     <div v-else="" class="text-red-500 text-sm">Please choose a dog first.</div>
-    <div  v-for="dog in $page.props.dogs" :key="dog.id"
-          @click="dogStore.chooseDog(dog.id)"
-          class="flex gap-4 pr-12 cursor-pointer"
-    >
-        {{ dog.name }}
+    <div class="flex gap-6 items-center">
+      <div  v-for="dog in $page.props.dogs" :key="dog.id"
+            @click="dogStore.chooseDog(dog.id)"
+            class="flex gap-4 pr-12 cursor-pointer"
+            :class="dogStore.chosenDog?.id == dog.id ? 'font-semibold text-xl' : ''"
+      >
+          {{ dog.name }}
+      </div>
     </div>
 
   </div>
