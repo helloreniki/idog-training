3fb960a20d8973a6b1f96d624ae0be976153c5b3
Reniki <hello@reniki.com>
2023-03-14 10:51:53 +0100

    SelectListboc component

--- resources/js/Pages/Skill/SkillView.vue
+++ resources/js/Pages/Skill/SkillView.vue
@@ -31,64 +31,8 @@
           <div v-if="showNewCategoryInput === false" class="w-full flex flex-col gap-4">
             <InputLabel for="category" value="Choose category" />
 
-            <div class="w-full">
-              <Listbox v-model="form.category">
-                <div class="relative mt-1">
-                  <ListboxButton
-                    class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left border border-gray-500 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
-                  >
-                    <span class="block truncate">{{ form.category ?? "Choose category..." }}</span>
-                    <span
-                      class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
-                    >
-                      <ChevronUpDownIcon
-                        class="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </ListboxButton>
+            <SelectListbox :options="categories" v-model="form.category" />
 
-                  <transition
-                    leave-active-class="transition duration-100 ease-in"
-                    leave-from-class="opacity-100"
-                    leave-to-class="opacity-0"
-                  >
-                    <ListboxOptions
-                      class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-                    >
-                      <ListboxOption
-                        v-slot="{ active, selected }"
-                        v-for="category, index in categories"
-                        :key="index"
-                        :value="category"
-                        as="template"
-                      >
-                        <li
-                          :class="[
-                            active ? 'bg-amber-100 text-amber-900' : 'text-gray-900',
-                            'relative cursor-default select-none py-2 pl-10 pr-4',
-                          ]"
-                        >
-                          <span
-                            :class="[
-                              selected ? 'font-medium' : 'font-normal',
-                              'block truncate',
-                            ]"
-                            >{{ category }}</span
-                          >
-                          <span
-                            v-if="selected"
-                            class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
-                          >
-                            <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        </li>
-                      </ListboxOption>
-                    </ListboxOptions>
-                  </transition>
-                </div>
-              </Listbox>
-            </div>
             <div v-if="form.errors.category" class="text-sm text-red-500">{{ form.errors.category }}</div>
 
             <SecondaryButton type="button" @click="showNewCategoryInput = true" class="self-start">or input new Category</SecondaryButton>
@@ -137,9 +81,9 @@ import TextInput from '@/Components/TextInput.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import SkillItem from '@/Pages/Skill/SkillItem.vue'
 import CategoriesTab from './CategoriesTab.vue';
-import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
 import { useForm, usePage, router } from '@inertiajs/vue3'
+import SelectListbox from '@/Components/SelectListbox.vue';
 
 
 const page = usePage()
