e40126f1c2e8f43818e0814f532db7a7776bf826
Reniki <hello@reniki.com>
2023-03-15 17:48:24 +0100

    sidebar toggle

--- resources/js/Parts/Sidebar.vue
+++ resources/js/Parts/Sidebar.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4 pr-3 lg:pr-8 min-h-screen min-w-fit">
-    <div class="flex flex-shrink-0 items-center space-y-5 px-4">
-      <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+  <div class="relative flex flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4 pr-3 min-h-screen min-w-fit">
+    <div @click="sidebarOpened = !sidebarOpened" class="py-2">
+      <XCircleIcon v-if="sidebarOpened" class="absolute top-2 right-2 w-6 h-6 text-violet-600 hover:text-violet-800" />
+      <EllipsisHorizontalIcon v-else class="absolute top-2 right-2 w-6 h-6 text-violet-600 hover:text-violet-800" />
     </div>
-    <div class="mt-5 flex flex-grow flex-col">
+    <div class="mt-5 sm:flex flex-grow flex-col">
       <nav class="flex-1 space-y-1 bg-white" aria-label="Sidebar">
-        <Link v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'border-indigo-600 bg-indigo-50 text-indigo-600' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center border-l-4 px-3 py-2 text-sm font-medium']">
+        <Link v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'border-indigo-600 bg-indigo-50 text-indigo-600' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center border-l-4 pl-3 pr-1 py-2 text-sm font-medium']">
           <component :is="item.icon" :class="[item.current ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 h-6 w-6 flex-shrink-0']" aria-hidden="true" />
-          {{ item.name }}
+          <div v-show="sidebarOpened" class="pr-2">{{ item.name }}</div>
         </Link>
       </nav>
     </div>
@@ -15,13 +16,16 @@
 </template>
 
 <script setup>
-import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon } from '@heroicons/vue/24/outline'
+import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon, XCircleIcon } from '@heroicons/vue/24/outline'
 import { useDogStore } from '../../Stores/dog.js'
 import { usePage } from '@inertiajs/vue3'
-import { computed, watch } from 'vue';
+import { computed, ref } from 'vue';
+import { EllipsisHorizontalIcon } from '@heroicons/vue/20/solid';
 
 const page = usePage()
-const dogStore = useDogStore()
+// const dogStore = useDogStore()
+
+const sidebarOpened = ref(true)
 
 const component = computed(() => page.component)
 
