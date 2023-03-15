0e687c39144326ccc6a836c3a4ecec9be3b5c6a2
Reniki <hello@reniki.com>
2023-03-15 18:01:37 +0100

    sidebar Transition

--- resources/js/Parts/Sidebar.vue
+++ resources/js/Parts/Sidebar.vue
@@ -8,7 +8,16 @@
       <nav class="flex-1 space-y-1 bg-white" aria-label="Sidebar">
         <Link v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'border-indigo-600 bg-indigo-50 text-indigo-600' : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center border-l-4 pl-3 pr-1 py-2 text-sm font-medium']">
           <component :is="item.icon" :class="[item.current ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-3 h-6 w-6 flex-shrink-0']" aria-hidden="true" />
-          <div v-show="sidebarOpened" class="pr-2">{{ item.name }}</div>
+          <Transition
+            enter-active-class="duration-300 ease-out"
+            enter-from-class="transform opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="duration-200 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="transform opacity-0"
+          >
+            <div v-show="sidebarOpened" class="pr-2">{{ item.name }}</div>
+          </Transition>
         </Link>
       </nav>
     </div>
